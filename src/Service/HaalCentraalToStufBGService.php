87ad807098652ccffe01d8cc0075d1e4b2e27be3
Barry Brands <barrybrands02@hotmail.com>
2023-06-28 17:18:07 +0200

    code added for fetching ingeschrevenpersonen

--- src/Service/HaalCentraalToStufBGService.php
+++ src/Service/HaalCentraalToStufBGService.php
@@ -8,8 +8,13 @@
 
 namespace CommonGateway\HaalCentraalToStufBGBundle\Service;
 
-use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Gateway as Source;
+use App\Entity\Mapping;
 use Psr\Log\LoggerInterface;
+use CommonGateway\CoreBundle\Service\GatewayResourceService;
+use CommonGateway\CoreBundle\Service\CallService;
+use CommonGateway\CoreBundle\Service\MappingService;
+use Symfony\Component\HttpFoundation\Response;
 
 class HaalCentraalToStufBGService
 {
@@ -23,11 +28,20 @@ class HaalCentraalToStufBGService
      * @var array
      */
     private array $data;
+    /**
+     * @var CallService
+     */
+    private CallService $callService;
 
     /**
-     * @var EntityManagerInterface
+     * @var MappingService
      */
-    private EntityManagerInterface $entityManager;
+    private MappingService $mappingService;
+
+    /**
+     * @var GatewayResourceService
+     */
+    private GatewayResourceService $gatewayResourceService;
 
     /**
      * The plugin logger.
@@ -38,19 +52,121 @@ class HaalCentraalToStufBGService
 
 
     /**
-     * @param EntityManagerInterface $entityManager The Entity Manager.
+     * @param CallService            $callService The CallService.
+     * @param MappingService         $mappingService The MappingService.
+     * @param GatewayResourceService $gatewayResourceService The GatewayResourceService.
      * @param LoggerInterface        $pluginLogger  The plugin version of the logger interface.
      */
     public function __construct(
-        EntityManagerInterface $entityManager,
+        CallService $callService,
+        MappingService $mappingService,
+        GatewayResourceService $gatewayResourceService,
         LoggerInterface $pluginLogger
     ) {
-        $this->entityManager = $entityManager;
-        $this->logger        = $pluginLogger;
-        $this->configuration = [];
-        $this->data          = [];
+        $this->callService            = $callService;
+        $this->mappingService         = $mappingService;
+        $this->gatewayResourceService = $gatewayResourceService;
+        $this->logger                 = $pluginLogger;
+        $this->configuration          = [];
+        $this->data                   = [];
 
     }//end __construct()
+
+
+    /**
+     * Gets BSN from xml body.
+     * 
+     * @return string|null $bsn.
+     */
+    private function getBsnFromBody(): ?string
+    {
+        return $this->data['body']['SOAP-ENV:Body']['BG:npsLv01-prs-GezinssituatieOpAdresAanvrager']['BG:gelijk']['BG:inp.bsn'] ?? null;
+    }
+
+    
+    /**
+     * Finds source by reference.
+     * 
+     * @return Source|null The resulting source.
+     */
+    public function getSource(): ?Source
+    {
+        $source = $this->gatewayResourceService->getSource('https://commongateway.nl/source/stuf.haalcentraal.source.json', 'haalcentraal-to-stufbg-bundle');
+        if ($source instanceof Source === false) {
+            $this->logger->error("No source found with reference: https://commongateway.nl/source/stuf.haalcentraal.source.json");
+
+            return null;
+        }
+        
+        return $source;
+    }//end getSource()
+
+    
+    /**
+     * Finds mapping by reference.
+     * 
+     * @return Mapping|null The resulting mapping.
+     */
+    public function getMapping(): ?Source
+    {
+        $mapping = $this->gatewayResourceService->getMapping('https://commongateway.nl/source/stuf.haalCentraalToLa01.source.json', 'haalcentraal-to-stufbg-bundle');
+        if ($mapping instanceof Source === false) {
+            $this->logger->error("No mapping found with reference: https://commongateway.nl/source/stuf.haalCentraalToLa01.source.json");
+
+            return null;
+        }
+        
+        return $mapping;
+    }//end getMapping()
+
+    
+    /**
+     * Fetches a person with given source and endpoint.
+     * 
+     * @return array|null The fetched person
+     */
+    public function fetchPerson(Source $source, string $endpoint): ?array
+    {
+        $this->logger->info('Fetching ingeschrevenpersoon..');
+        try {
+            $response = $this->callService->call($source, $endpoint, 'GET');
+            return $this->callService->decodeResponse($source, $response, 'application/json');
+        } catch (\Exception $e) {
+            // Logging might be dangerous here?
+            // $this->logger->error('Error when fetching ingeschrevenpersoon: ' . $e->getMessage());
+
+            return null;
+        }
+    }//end fetchPerson()
+
+    
+    /**
+     * Fetches all relatives of the ingeschreven persoon.
+     * 
+     * @param Source $source
+     * @param array $ingeschrevenPersoon
+     * 
+     * @return array|null The relatives of a ingeschreven persoon.
+     */
+    public function getAllRelatives(Source $source, array $ingeschrevenPersoon): ?array
+    {
+        $fetchedPeople = ['partners' => [], 'ouders' => [], 'kinderen' => []];
+        foreach ($fetchedPeople as $type => $people) {
+            if (isset($ingeschrevenpersoon['_links'][$type]) === true) {
+                foreach ($ingeschrevenPersoon['_links'][$type] as $link) {
+                    // Remove domain etc from link so we have a endpoint.
+                    $endpoint = str_replace($link, '', $source->getLocation());
+                    $fetchedPeople[$type][] = $this->fetchPerson($source, $endpoint);
+                }
+            }
+        }
+        
+        return [
+            'enrichedPartners' => $fetchedPeople['partners'],
+            'enrichedParents' => $fetchedPeople['ouders'],
+            'enrichedChildren' => $fetchedPeople['kinderen'],
+        ];
+    }//end getAllRelatives()
 
 
     /**
@@ -63,12 +179,43 @@ class HaalCentraalToStufBGService
      */
     public function haalCentraalToStufBGHandler(array $data, array $configuration): array
     {
+        $this->logger->debug("HaalCentraalToStufBGService -> HaalCentraalToStufBGHandler()");
         $this->data          = $data;
         $this->configuration = $configuration;
 
-        $this->logger->debug("HaalCentraalToStufBGService -> HaalCentraalToStufBGHandler()");
+        // 0. Validate some configuration.
+        $mapping = $this->getMapping();
+        if ($mapping === null) {
 
-        return ['response' => 'Hello. Your HaalCentraalToStufBGBundle works'];
+            return [];
+        }
+
+        $source = $this->getSource();
+        if ($source === null) {
+
+            return [];
+        }
+
+        // 1. Get bsn from body.
+        $this->logger->info('Getting BSN from request body..');
+        $bsn = $this->getBsnFromBody();
+        if ($bsn === null) {
+            $this->logger->error('BSN not found in xml body.');
+
+            return [];
+        }
+        // 2. Get ingeschrevenpersoon from source. 
+        $ingeschrevenPersoon = $this->fetchPerson($source, "/$bsn");
+
+        // 3. Check partners, parents and children. Fetch those. 
+        $allRelatives = $this->getAllRelatives($source, $ingeschrevenPersoon);
+
+        // 4. Map them together into a stuf response. 
+        $allRelatives = array_merge($allRelatives, $ingeschrevenPersoon);
+        $stufXml = $this->mappingService->mapping($mapping, $allRelatives);
+
+
+        return ['response' => new Response(json_encode($stufXml), 200, ['Accept' => 'application/xml'])];
 
     }//end haalCentraalToStufBGHandler()
 
